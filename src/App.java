77327466f8cbe48ae61a0da4bf192a3d2d8b1d68
Jesiel Viana <jesielviana@proton.me>
2023-11-02 09:59:21 -0300

    Feedback sobre a atividade

--- src/App.java
+++ src/App.java
@@ -2,34 +2,41 @@ import java.sql.Date;
 
 public class App {
     public static void main(String[] args) {
-        Endereco endereco1 = new Endereco("Avenida Pedro Marques de Medeiros", 10, "Pantanal", "Picos", "PI");
+        Endereco endereco1 =
+                new Endereco("Avenida Pedro Marques de Medeiros", 10, "Pantanal", "Picos", "PI");
         Cliente cliente1 = new Cliente("12345678910", "João", new Date(2000, 1, 1));
 
-        Conta contaCorrente = new ContaCorrente(123, 001, 100.00, "corrente", cliente1, new NotificacaoEmail(), 50.00, 2);
-        Conta contaPoupanca = new ContaPoupanca(321, 002, 200.00, "poupança", cliente1, new NotificacaoSMS(),0.1);
+        Conta cc = new ContaCorrente(123, 001, 0.00, "corrente", cliente1, new NotificacaoEmail(),
+                50.00, 2);
+        Conta cc2 = new ContaCorrente(123, 001, 0.00, "corrente", cliente1, new NotificacaoEmail(),
+                50.00, 2);
+        Conta cp =
+                new ContaPoupanca(321, 002, 0.00, "poupança", cliente1, new NotificacaoSMS(), 0.1);
+        System.out.println(cc.getSaldo()); // o valor do cheque especial está sendo exibido como
+                                           // saldo, não deveria
+        cc.depositar(50);
+        System.out.println(cc.getSaldo());
+        cc.sacar(100);
+        // nesse exemplo era pra ficar com saldo -50, mas como o cheque especial foi somando ao
+        // saldo no inicio ficou o saldo 0
+        System.out.println(cc.getSaldo());
+        // cc.transferencia(cc2, 10);
+        // cc.transferencia(cc2, 10);
+        // cc.transferencia(cc2, 10);
+        // cc.transferencia(cc2, 10);
+        // cc.transferencia(cc2, 10);
+        // cc.transferencia(cc2, 50);
+        // System.out.println(cc2.getSaldo());
+        // System.out.println(cc.getSaldo());
+        // cc.exibeExtrato();
 
-        // Métodos da conta corrente
-        System.out.println("\n");
-        contaCorrente.depositar(100);
-        System.out.println("\n");
-        contaCorrente.sacar(50);
-        System.out.println("\n");
-        contaCorrente.transferencia(contaPoupanca, 100);
-        System.out.println("\n");
-        contaCorrente.transferencia(contaPoupanca, 25);
-        System.out.println("\n");
-        contaCorrente.transferencia(contaPoupanca, 25);
-        System.out.println("\n");
-        contaCorrente.exibeExtrato();
+        // cp.depositar(100);
+        // System.out.println(cp.getSaldo());
+        // cp.sacar(10);
+        // System.out.println(cp.getSaldo());
+        // cp.exibeExtrato();
 
-        // Métodos da conta poupança
-        System.out.println("\n");
-        contaPoupanca.depositar(50);
-        System.out.println("\n");
-        contaPoupanca.sacar(125);
-        System.out.println("\n");
-        contaPoupanca.transferencia(contaCorrente, 75);
-        System.out.println("\n");
-        contaPoupanca.exibeExtrato();
+        // só encontrei o problema com a manipulação do cheque especial, mas estruturalmente está
+        // tudo certo.
     }
 }
--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,10 +1,15 @@
 import java.time.LocalDate;
 
-public class ContaCorrente extends Conta{
+public class ContaCorrente extends Conta {
     private double chequeEspecial;
     private int transferenciaSemTaxa = 2;
 
-    public ContaCorrente(int agencia, int numero, double saldo, String tipoConta, Cliente cliente, Notificacao notificacao, double chequeEspecial, int transacaoSemTaxa) {
+    public ContaCorrente(int agencia, int numero, double saldo, String tipoConta, Cliente cliente,
+            Notificacao notificacao, double chequeEspecial, int transacaoSemTaxa) {
+        // saldo + chequeEspecial -> não deve somar o cheque especial ao saldo, não duas coisas
+        // diferentes.
+        // cheque especial é um limite de emprestimo que vc pode pegar junto ao banco e ficar com
+        // saldo negativo
         super(agencia, numero, saldo + chequeEspecial, tipoConta, cliente, notificacao);
         this.chequeEspecial = chequeEspecial;
         this.transferenciaSemTaxa = 2;
@@ -31,20 +36,21 @@ public class ContaCorrente extends Conta{
 
     @Override
     public void transferencia(Conta contaDestino, double valor) {
-        if (valor > 0 && valor <= saldo + chequeEspecial){
+        if (valor > 0 && valor <= saldo + chequeEspecial) {
             this.setSaldo(this.getSaldo() - valor);
             contaDestino.setSaldo(contaDestino.getSaldo() + valor);
-            System.out.println("Você transferiu: R$" + valor + " da sua conta corrente para a sua conta poupança.");
-            
-            if (transferenciaSemTaxa <= 0){
+            System.out.println("Você transferiu: R$" + valor
+                    + " da sua conta corrente para a sua conta poupança.");
+
+            if (transferenciaSemTaxa <= 0) {
                 double taxa = valor * 0.1;
                 this.setSaldo(this.getSaldo() - taxa);
             }
-            
+
             Movimentacao movimentacao = new Movimentacao(LocalDate.now(), valor, "tranferência");
             historicoMovimentacao.add(movimentacao);
 
-            transferenciaSemTaxa --;    
+            transferenciaSemTaxa--;
             System.out.println("Seu saldo atual é: R$" + this.getSaldo());
 
             System.out.println("\n");
